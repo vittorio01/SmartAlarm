674d7662ec39fb8c4578e232ed09908860c0b3eb
vittorio01 <vittoriopolci@live.com>
2023-02-19 22:39:56 +0100

    aggiornata la struttura base della sezione dedicata all'hardware

--- modules/hardware/hardware.c
+++ modules/hardware/hardware.c
@@ -1,6 +1,7 @@
 #include "hardware.h"
 
 void initHardware() {
+    initClockSystem();
     initTimerSystem();
 
 }
@@ -10,4 +11,9 @@ void initTimerSystem() {
     timerlist.timer1_used=false;
     timerlist.timer2_used=false;
     timerlist.timer3_used=false;
+}
+
+void initClockSystem() {
+    CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
+    CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
 }
