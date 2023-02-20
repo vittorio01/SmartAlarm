c89c06fa76bf129a36cbd87c41b0e41e0d8e654c
vittorio01 <vittoriopolci@live.com>
2023-02-20 15:10:12 +0100

    modifiche generali

--- modules/hardware/hardware.c
+++ modules/hardware/hardware.c
@@ -6,7 +6,7 @@ void initHardware() {
 
 }
 
-void initTimerSystem() {
+void initTimearSystem() {
     timerlist.timer0_used=false;
     timerlist.timer1_used=false;
     timerlist.timer2_used=false;
