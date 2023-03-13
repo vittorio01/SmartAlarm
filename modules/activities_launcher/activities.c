6aa5b3e0e0b61ffbc9876a1c2f74ddb8ceb0fc70
ilmonk <ennio.monchelato@studenti.unitn.it>
2023-03-13 16:05:49 +0100

    activities launcher
    funzione che gestisce il lancio delle attività (manca il passaggio del graphic context)

--- modules/activities_launcher/activities.c
+++ modules/activities_launcher/activities.c
@@ -1,9 +1,13 @@
 #include "activities.h"
 
-/*
+
 
 state launch_game(){
+    const int activitiesNumber = 1;
+    func_type activityList[activitiesNumber];
+    time_t t;
+    srand((unsigned) time(&t));
     int i = rand() % activitiesNumber;
-    return activityList[i]();
+    return activityList[i]();//manca il passaggio del graphic context
 
-}*/
+}
--- modules/activities_launcher/activities.h
+++ modules/activities_launcher/activities.h
@@ -2,12 +2,12 @@
 #define __ACTIVITIES_LAUNCHER__
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef enum {RUNNING, USER_INACTIVE, TASK_COMPLETED, TASK_CLOSED, UNDEFINED} state;
-typedef state (*func_type)(void);
+typedef state (*func_type)(); //manca il passaggio del graphic context 
 
 state launch_game();
-//const int activitiesNumber = 1;
-//state (*activityList[activitiesNumber])();
-//func_type activityList[1];
+
 #endif
