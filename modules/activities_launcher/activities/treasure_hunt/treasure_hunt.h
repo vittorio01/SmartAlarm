085254fa1ad632caaee17d96e693a86f4557e401
Ennio Monchelato <95639617+ilmonk@users.noreply.github.com>
2023-06-12 16:00:43 +0200

    Update treasure_hunt.h

--- modules/activities_launcher/activities/treasure_hunt/treasure_hunt.h
+++ modules/activities_launcher/activities/treasure_hunt/treasure_hunt.h
@@ -7,21 +7,7 @@
 #include <math.h>
 #include <time.h>
 
-/*
- * Combo master!
- *
- * To win this minigame the user has to repeat a given combination of movements in the correct order in a specific time interval.
- * The combination is visualized only once at game start. All possible movements and actions are:
- * -    up_arrow -> a vertical movement from the center to the upper position of the analog joypad
- * -    down_arrow -> a vertical movement from the center to the lower position of the analog joypad
- * -    left_arrow -> a horizontal movement from the center to the max left position of the analog joypad
- * -    right_arrow -> a horizontal movement from the center to the max right position of the analog joypad
- * -    circle_counter_wise -> a circular movement which start from the upper position, travel to the edge and returns to the upper position of the joypad
- * -    circle_wise_clock -> same as circle_counter_wise but in the opposite direction
- *
- * -    A -> a pressure of the first button is used for send the combination
- * -    B -> a pressure of the second button is used for delete the last action
- */
+
 
 state currentState;
 
